3e972a8a37433406cbf7e05856fd6f7c8987e860
Paco <lysar1x950@gmail.com>
2025-09-17 23:31:55 -0600

    Component created

---
+++ src/app/convertidor/convertidor.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-convertidor',
+  imports: [],
+  templateUrl: './convertidor.html',
+  styleUrl: './convertidor.css'
+})
+export class Convertidor {
+
+}
