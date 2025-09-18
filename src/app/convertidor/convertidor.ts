619abc8ae48f76a7e679bc85787d6e976cb46947
Paco <lysar1x950@gmail.com>
2025-09-17 23:42:40 -0600

    Modified typescript

--- src/app/convertidor/convertidor.ts
+++ src/app/convertidor/convertidor.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-convertidor',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './convertidor.html',
   styleUrl: './convertidor.css'
 })
 export class Convertidor {
-
+  metros: number=0;
+  pies: number=0;
 }
